48a23110f879d08f3c77264bdab11d4b8fec41b9
brunogrecco <bgrecco@udesa.edu.ar>
2020-11-19 18:57:10 -0300

    detalles

---
+++ detalles.js
@@ -0,0 +1,8 @@
+//llamar a la appi
+
+//llamar a title
+//fetch llamando al id de la pelicula y modificando el title que llame arriba
+
+
+//window. onload
+//llamar a todos los elementos (index search fav) (titulo, duracion, descripcion, etc)
